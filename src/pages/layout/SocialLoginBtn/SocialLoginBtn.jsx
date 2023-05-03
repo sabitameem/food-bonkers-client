c2f3049250c820522bde5fdcff9bee9224c97f03
sabitameem <sabitameem578@gmail.com>
2023-05-04 03:14:00 +0530

    github loggedin solved

--- src/pages/layout/SocialLoginBtn/SocialLoginBtn.jsx
+++ src/pages/layout/SocialLoginBtn/SocialLoginBtn.jsx
@@ -1,60 +1,72 @@
-import React, { useState } from 'react';
-import './SocialLoginBtn.css';
-import { GoogleAuthProvider,getAuth,signInWithPopup,GithubAuthProvider, } from "firebase/auth";
-import app from '../../../firebase/firebase.config';
+import React, { useState } from "react";
+import "./SocialLoginBtn.css";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  GithubAuthProvider,
+} from "firebase/auth";
+import app from "../../../firebase/firebase.config";
 
 const SocialLoginBtn = () => {
-    const [user, setUser] = useState({});
-    const auth = getAuth(app);
-    const googleProvider = new GoogleAuthProvider();
-    const githubProvider = new GithubAuthProvider();
-    
-    const handleGoogleLogin = () => {
-        signInWithPopup(auth, googleProvider)
-          .then((result) => {
-            const user = result.user;
-            setUser(user);
-            console.log(user);
-          })
-          .catch((error) => {
-            const errorMessage = error.message;
-            console.log(errorMessage);
-          });
-        };
+  const [user, setUser] = useState({});
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
-          const handleGithubLogin = () => {
-            signInWithPopup(auth, githubProvider)
-              .then((result) => {
-                const user = result.user;
-                console.log(user);
-              })
-              .catch((error) => {
-                const errorMessage = error.message;
-              });
-      };
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
 
-     
+  const handleGithubLogin = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GithubAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
 
-    return (
-        <div className=" social-button-container w-50 mt-3">
-        <div className="">
-          <img
-            onClick={handleGoogleLogin}
-            className=" social-button"
-            src="https://i.ibb.co/gSTHXZJ/google-btn.png"
-            alt=""
-          />
-        </div>
-        <div className="">
-          <img
-            onClick={handleGithubLogin}
-            className=" social-button"
-            src="https://i.ibb.co/g9f4P0S/github-btn.png"
-            alt=""
-          />
-        </div>
+  return (
+    <div className=" social-button-container w-50 mt-3">
+      <div className="">
+        <img
+          onClick={handleGoogleLogin}
+          className=" social-button"
+          src="https://i.ibb.co/gSTHXZJ/google-btn.png"
+          alt=""
+        />
       </div>
-    );
+      <div className="">
+        <img
+          onClick={handleGithubLogin}
+          className=" social-button"
+          src="https://i.ibb.co/g9f4P0S/github-btn.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SocialLoginBtn;
