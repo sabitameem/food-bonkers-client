a1f68ab92bea071772b40796fde06c30b7f6516a
sabitameem <sabitameem578@gmail.com>
2023-05-04 11:11:31 +0530

    typing fixed

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -7,13 +7,13 @@ const Banner = () => {
         <h2 className='pt-3 d-none  d-lg-block' style={{fontSize: "70px" , fontWeight:'bolder'}}>Discover your <br/><span className='text-warning'> Own</span> taste!!!</h2>
         <h2 className='pt-1 d-lg-none' style={{fontSize: "30px" , fontWeight: 'bold'}}>Discover your <br/><span className='text-warning'> Own</span> taste!!!</h2>
 
-        <h2 className=' d-none  d-lg-block' style={{fontSize: '25px'}}>We try to show you all the variety of Japanese cuisine and its current trends.</h2>
+        <h2 className=' d-none  d-lg-block' style={{fontSize: '25px'}}>We try to show you all the variety of American cuisine and its current trends.</h2>
         
-        <h2 className=' d-lg-none' style={{fontSize: '10px'}}>We try to show you all the variety of Japanese cuisine and its current trends.</h2>
+        <h2 className=' d-lg-none' style={{fontSize: '10px'}}>We try to show you all the variety of American cuisine and its current trends.</h2>
 
         </div>
-        <img className='d-none w-50 d-lg-block' style={{height: '400px'}} src="https://i.ibb.co/16bDnJL/banner3.webp" alt="" />
-        <img className='w-50 d-lg-none'style={{ height: '200px' }} src="https://i.ibb.co/16bDnJL/banner3.webp" alt="" />
+        <img className='d-none w-50 d-lg-block rounded' style={{height: '400px'}} src="https://i.ibb.co/16bDnJL/banner3.webp" alt="" />
+        <img className='w-50 d-lg-none rounded'style={{ height: '200px' }} src="https://i.ibb.co/16bDnJL/banner3.webp" alt="" />
         
     </div>
     );
