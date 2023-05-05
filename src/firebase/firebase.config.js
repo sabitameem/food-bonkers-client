586df666e26c3e515ba93485f08377843b7e41b0
sabitameem <sabitameem578@gmail.com>
2023-05-05 11:52:17 +0530

    firebase confige hide

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+console.log(import.meta.env.VITE_apiKey)
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCfvmfpKYcFbaQ6vNYnpGoZsui4F8gSWvg",
-  authDomain: "food-bonkers-website.firebaseapp.com",
-  projectId: "food-bonkers-website",
-  storageBucket: "food-bonkers-website.appspot.com",
-  messagingSenderId: "1018661139903",
-  appId: "1:1018661139903:web:a7111b04b2c7cbb5d85d0a"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket ,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId 
 };
 
 // Initialize Firebase
